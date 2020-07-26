485a01314fa5c2e95529b49696258e1d4d5ee0d3
benini17 <67801606+benini17@users.noreply.github.com>
2020-07-26 15:53:13 -0300

    Update desafio1API.js
    init

--- desafio1API.js
+++ desafio1API.js
@@ -1,19 +1,13 @@
-inputName.addEventListener('keyup', messageUsersFound);
-
-const start = () => {
-  console.log('Página totalmente carregada');
-  inputName = document.querySelector('#inputName');
-  let searchBtn = document.getElementById('searchBtn');
-  searchBtn.disabled = true;
-  preventFormSubmit();
-  form();
-};
-
-let object = null;
-
 function fetchPromise() {
   fetchUsers();
 }
+function display() {
+  statistics = document.getElementById('statistics');
+  statistics.style.display = 'none';
+}
+
+display();
+fetchPromise();
 
 async function fetchUsers() {
   const response = await fetch(
@@ -21,7 +15,8 @@ async function fetchUsers() {
   );
   console.log(response);
   const json = await response.json();
-  const object = json.results;
+  let object = json.results;
+  object = object.sort((a, b) => a.name.first.localeCompare(b.name.first));
 
   console.log(object);
 
@@ -31,10 +26,151 @@ async function fetchUsers() {
     )
   );
   console.log(globalInfo);
-}
 
-fetchPromise();
-start();
+  const start = () => {
+    console.log('Página totalmente carregada');
+    inputName = document.querySelector('#inputName');
+    let searchBtn = document.getElementById('searchBtn');
+    searchBtn.disabled = true;
+    preventFormSubmit();
+    form(object, globalInfo);
+  };
+
+  start();
+
+  inputName.addEventListener('keyup', messageUsersFound);
+
+  function messageUsersFound(event) {
+    let inputNameValue = event.target.value;
+    let hasText = !!inputNameValue && inputNameValue.trim() !== '';
+
+    if (hasText) {
+      searchBtn.disabled = false;
+      search();
+    }
+  }
+
+  function statisticsCounter(otherInfo) {
+    console.log('otherinfo inicio statistics', otherInfo);
+    let shownUsers = Array.from(
+      document.getElementById('names').querySelectorAll('#shown')
+    );
+
+    // let object = people.results;
+
+    let globalInfo = object.map((person, index) =>
+      JSON.parse(
+        `{"fullname":"${person.name.first} ${person.name.last}", "age": "${person.dob.age}", "gender": "${person.gender}", "index": "${index}"}`
+      )
+    );
+
+    console.log('shown users teste', shownUsers);
+
+    let arrFoundUsers = shownUsers.map((li) => li.textContent.split(',', 1));
+
+    console.log('index do pessoal aqui', arrFoundUsers);
+
+    const arrFilteredFoundUsers = globalInfo.filter((person) => {
+      for (let i = 0; i < arrFoundUsers.length; i++) {
+        if (person.fullname == arrFoundUsers[i]) {
+          return person;
+        }
+      }
+    });
+
+    console.log('arrFilteredFoundUsers', arrFilteredFoundUsers.length);
+    let totalSum = 0;
+
+    console.log('otherinfo', otherInfo);
+
+    if (otherInfo == 'male') {
+      let numMen = arrFilteredFoundUsers.reduce(function (n, person) {
+        return n + (person.gender == 'male');
+      }, 0);
+      console.log('numMen', numMen);
+      return numMen;
+    } else if (otherInfo == 'female') {
+      let numWomen = arrFilteredFoundUsers.reduce(function (n, person) {
+        return n + (person.gender == 'female');
+      }, 0);
+      console.log('numWomen', numWomen);
+      return numWomen;
+    } else if (otherInfo == 'ageSum') {
+      const sumAge = arrFilteredFoundUsers.reduce((acc, curr) => {
+        return parseInt(acc) + parseInt(curr.age);
+      }, 0);
+      console.log('sumAge', sumAge);
+      return sumAge;
+    } else {
+      const sum = arrFilteredFoundUsers.reduce((acc, curr) => {
+        return parseInt(acc) + parseInt(curr.age);
+      }, 0);
+      if (sum > 0) {
+        const avarage = sum / arrFilteredFoundUsers.length;
+        console.log('avarage', avarage);
+        return parseFloat(avarage.toFixed(2));
+      } else {
+        return 0;
+      }
+    }
+  }
+
+  function search() {
+    function startSearch(event) {
+      const searchFilterBtnApply = () =>
+        Array.from(list.children).map((li) => {
+          let matchFound = new RegExp(inputName.value, 'gi').test(li.innerText);
+
+          if (matchFound) {
+            li.id = 'shown';
+
+            let someUsers = document.getElementById('someUsers');
+
+            zeroUsers.style.display = 'none';
+            someUsers.style.display = 'initial';
+          } else {
+            li.classList.add('hidden');
+            li.id = '';
+          }
+          someUsers.innerHTML = `${counter()} found users`;
+        });
+
+      searchFilterBtnApply();
+      statisticSection();
+    }
+
+    searchBtn.addEventListener('click', search);
+
+    inputName.addEventListener('keyup', (event) => {
+      let inputNameValue = event.target.value;
+      let hasText = !!inputNameValue && inputNameValue.trim() !== '';
+      if (hasText) {
+        if (event.key === 'Enter') {
+          startSearch();
+        } else {
+          return;
+        }
+      }
+    });
+  }
+
+  function statisticSection() {
+    let arrParagraph = Array.from(document.getElementsByClassName('answer'));
+
+    arrParagraph[0].textContent = statisticsCounter('male');
+    arrParagraph[1].textContent = statisticsCounter('female');
+    arrParagraph[2].textContent = statisticsCounter('ageSum');
+    arrParagraph[3].textContent = statisticsCounter('ageAverage');
+
+    if (arrParagraph[2].textContent > 0) {
+      let statistics = document.getElementById('statistics');
+      let noUsersFound = document.getElementById('noUsersFound');
+
+      statistics.style.display = 'initial';
+      noUsersFound.style.display = 'none';
+    }
+  }
+}
 
 function preventFormSubmit() {
   function stopSubmit(event) {
@@ -42,16 +178,6 @@ function preventFormSubmit() {
   }
   let form = document.querySelector('form');
   form.addEventListener('submit', stopSubmit);
-}
-
-function messageUsersFound(event) {
-  let inputNameValue = event.target.value;
-  let hasText = !!inputNameValue && inputNameValue.trim() !== '';
-
-  if (hasText) {
-    searchBtn.disabled = false;
-    search();
-  }
 }
 
 function messageUsersNotFound() {
@@ -62,116 +188,12 @@ function messageUsersNotFound() {
   someUsers.style.display = 'none';
 }
 
-function statisticsCounter(otherInfo) {
-  console.log('otherinfo inicio statistics', otherInfo);
-  let shownUsers = Array.from(
-    document.getElementById('names').querySelectorAll('#shown')
-  );
-
-  // let object = people.results;
-
-  let globalInfo = object.map((person, index) =>
-    JSON.parse(
-      `{"fullname":"${person.name.first} ${person.name.last}", "age": "${person.dob.age}", "gender": "${person.gender}", "index": "${index}"}`
-    )
-  );
-
-  console.log('shown users teste', shownUsers);
-
-  let arrFoundUsers = shownUsers.map((li) => li.textContent.split(',', 1));
-
-  console.log('index do pessoal aqui', arrFoundUsers);
-
-  const arrFilteredFoundUsers = globalInfo.filter((person) => {
-    for (let i = 0; i < arrFoundUsers.length; i++) {
-      if (person.fullname == arrFoundUsers[i]) {
-        return person;
-      }
-    }
-  });
-
-  console.log('arrFilteredFoundUsers', arrFilteredFoundUsers.length);
-  let totalSum = 0;
-
-  console.log('otherinfo', otherInfo);
-
-  if (otherInfo == 'male') {
-    let numMen = arrFilteredFoundUsers.reduce(function (n, person) {
-      return n + (person.gender == 'male');
-    }, 0);
-    console.log('numMen', numMen);
-    return numMen;
-  } else if (otherInfo == 'female') {
-    let numWomen = arrFilteredFoundUsers.reduce(function (n, person) {
-      return n + (person.gender == 'female');
-    }, 0);
-    console.log('numWomen', numWomen);
-    return numWomen;
-  } else if (otherInfo == 'ageSum') {
-    const sumAge = arrFilteredFoundUsers.reduce((acc, curr) => {
-      return parseInt(acc) + parseInt(curr.age);
-    }, 0);
-    console.log('sumAge', sumAge);
-    return sumAge;
-  } else {
-    const sum = arrFilteredFoundUsers.reduce((acc, curr) => {
-      return parseInt(acc) + parseInt(curr.age);
-    }, 0);
-    if (sum > 0) {
-      const avarage = sum / arrFilteredFoundUsers.length;
-      console.log('avarage', avarage);
-      return parseFloat(avarage.toFixed(2));
-    } else {
-      return 0;
-    }
-  }
-}
-
 function counter(otherInfo) {
   let totalSum = document.querySelectorAll('#shown').length;
   return totalSum;
 }
 
-function search() {
-  function startSearch(event) {
-    const searchFilterBtnApply = () =>
-      Array.from(list.children).map((li) => {
-        let matchFound = new RegExp(inputName.value, 'gi').test(li.innerText);
-
-        if (matchFound) {
-          li.id = 'shown';
-
-          let someUsers = document.getElementById('someUsers');
-
-          zeroUsers.style.display = 'none';
-          someUsers.style.display = 'initial';
-        } else {
-          li.classList.add('hidden');
-          li.id = '';
-        }
-        someUsers.innerHTML = `${counter()} found users`;
-      });
-
-    searchFilterBtnApply();
-    statisticSection();
-  }
-
-  searchBtn.addEventListener('click', startSearch);
-
-  inputName.addEventListener('keyup', (event) => {
-    let inputNameValue = event.target.value;
-    let hasText = !!inputNameValue && inputNameValue.trim() !== '';
-    if (hasText) {
-      if (event.key === 'Enter') {
-        startSearch();
-      } else {
-        return;
-      }
-    }
-  });
-}
-
-function form() {
+function form(object, globalInfo) {
   //creating list structure and adding the information
   function creatingList() {
     divList = document.getElementById('names');
@@ -201,21 +223,22 @@ function form() {
       photo.src = peoplePhotos[index].thumbnail;
       return photo;
     }
-
     messageUsersNotFound();
   }
+  console.log('object in form', object);
+  console.log('object in form', globalInfo);
 
   //  List of people.js with all information alphabetically organized
-  let object = people.results;
-  object = object.sort((a, b) => a.name.first.localeCompare(b.name.first));
+  // let object = people.results;
+  // object = object.sort((a, b) => a.name.first.localeCompare(b.name.first));
 
-  console.log(object);
+  // console.log(object);
 
-  let globalInfo = object.map((person, index) =>
-    JSON.parse(
-      `{"fullname":"${person.name.first} ${person.name.last}", "age": "${person.dob.age}", "gender": "${person.gender}", "index": "${index}"}`
-    )
-  );
+  // let globalInfo = object.map((person, index) =>
+  //   JSON.parse(
+  //     `{"fullname":"${person.name.first} ${person.name.last}", "age": "${person.dob.age}", "gender": "${person.gender}", "index": "${index}"}`
+  //   )
+  // );
 
   //list of names and ages
   let shownPerson = globalInfo.map(
@@ -226,15 +249,6 @@ function form() {
   let peoplePhotos = object.map((personPhotos) => personPhotos.picture);
 
   creatingList();
-}
-
-function statisticSection() {
-  let arrParagraph = Array.from(document.getElementsByClassName('answer'));
-
-  arrParagraph[0].textContent = statisticsCounter('male');
-  arrParagraph[1].textContent = statisticsCounter('female');
-  arrParagraph[2].textContent = statisticsCounter('ageSum');
-  arrParagraph[3].textContent = statisticsCounter('ageAverage');
 }
 
 /* 
